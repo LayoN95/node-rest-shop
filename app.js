cba5b8d1289c138b50da9cb58622bf112f214aae
LayoN95 <mborowczyk95@gmail.com>
2019-06-09 20:34:44 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const ordersRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+
+mongoose.connect('mongodb+srv://marcin:' +
+process.env.MONGO_ATLAS_PW +
+'@nodejs-jamoh.mongodb.net/test?retryWrites=true&w=majority',
+{
+  useNewUrlParser: true
+});
+
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allowed-Origin', '*');
+  res.header('Access-Control-Allowed-Headers',
+   'Origin, X-Request-With, Content-Type, Accept, Authorization'
+ );
+ if (req.method === 'OPTIONS') {
+   res.header('Access-Contol-Allowed-Methods', 'PUT, POST, PATCH, DELETE, GET');
+   return res.status(200).json({});
+ }
+ next();
+});
+
+//Routes which should handle request
+app.use('/products', productRoutes);
+app.use('/orders', ordersRoutes);
+app.use('/user', userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) =>{
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+module.exports = app;
