7a1200977c87bf5afc49ad25486d3c0167e73a05
LayoN95 <mborowczyk95@gmail.com>
2019-06-13 18:13:21 +0200

    update

--- api/middleware/get-temp.js
+++ api/middleware/get-temp.js
@@ -1,11 +1,12 @@
 const ds18b20 = require('ds18b20-raspi');
 var temp = 0;
+
     ds18b20.readSimpleC((err, temperature) =>{
         if (err) {
             console.log(err);
             console.log("Brak urządzenia");
         } else {
-           
+            console.log(temperature);
             temp = temperature;
         }
     });
